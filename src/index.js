a6cd082615afebeb3991af4e19301f87edf3edee
apustovoitova <anastasiapustovoitova@gmail.com>
2023-04-08 02:59:19 +0200

    Lesson 13

--- src/index.js
+++ src/index.js
@@ -23,3 +23,20 @@ import Swiper, { Navigation, Pagination } from 'swiper';
     },
     modules: [Navigation, Pagination],
   });
+
+  var swiper = new Swiper(".trendsSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
